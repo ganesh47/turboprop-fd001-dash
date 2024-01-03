0c6404cdc44fa460475e69b064b9f703ba288039
graman47 <22994026+ganesh47@users.noreply.github.com>
2024-01-04 01:36:15 +0530

    GH pages publish

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
-let basePath = '/'
+let basePath = ''
 
 if (isGithubActions) {
-    // trim off `<owner>/`
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
     assetPrefix = `/${repo}/`
     basePath = `/${repo}`
 }
